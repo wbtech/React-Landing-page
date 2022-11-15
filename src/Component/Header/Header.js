665b434ebe6597b03201b8dc10b68143c672ac02
Wale Osho <90667947+wbtech@users.noreply.github.com>
2022-11-15 23:09:43 +0100

    Update Header.js

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,31 +1,52 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import Navbar from './Navbar';
+import { Menu } from '@material-ui/icons';
 
 const styles = {
     display:'flex', 
     justifyContent:'space-between',
-    alignItems: 'center' ,
-    backgroundColor: 'lightblue',
+     alignItems: 'center' ,
+    backgroundColor: 'orange',
     padding: '.5rem',
     width: "1000px",
     height: '40px',
-    color: 'blue',
+    color: 'white',
     boxShadow: '2px 2px 4px black'
 
 }
+let menu = document.querySelector('#menu')
+let navbar = document.querySelector('.navbar')
+console.log(navbar)
+console.log(menu)
+
+menu.addEventListener('click', function() {
+  navbar.classList.toggle('hide');
+})
+
+
 
 const Header = () => {
+
+function keysUp() {
+  let aides = document.querySelector('#aides').value
+  document.getElementById('aide').innerHTML = aides;
+
+}
+
   return (
-    <div style ={{display:'flex', justifyContent:'center', alignItems:'center'}} >
+    <div className='header' >
+       
         <div style= {styles}>
             <h2> Logo </h2>
             <input type='search'
             placeholder="search"
+            id='aides'
+            onKeyUp={keysUp}
             />
             <Navbar/>
-            
+          <div id='menu'><Menu /></div>  
         </div>  
-        
     </div>
   )
 }
