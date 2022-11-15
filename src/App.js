726a30f4fc08f0afb4910ab48f2c728d9a33bbb2
Wale Osho <90667947+wbtech@users.noreply.github.com>
2022-11-16 00:41:23 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react'
 import './App.css'
 import Header from './Component/Header/Header';
 // import LoginPage from './Component/Forms/LoginPage';
+import { ShoppingCart } from '@material-ui/icons';
 
 
 
@@ -12,6 +13,9 @@ const App = () => {
   return (
     <>
    <Header />
+   <p id='aide'>p</p>
+   <ShoppingCart className='cart' id="cart" />
+  
     {/* <LoginPage /> */}
     
     </>
